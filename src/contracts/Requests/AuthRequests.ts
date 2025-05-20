f07d8854ff9e851b2ee3900d3e8bdc265ceaf27c
DenilsonCordeiro <denilsoncordeiro0@gmail.com>
2025-05-20 06:08:38 -0300

    auth requests contracts

--- src/contracts/Requests/AuthRequests.ts
+++ src/contracts/Requests/AuthRequests.ts
@@ -1,9 +1,43 @@
 export interface LoginRequest {
   email: string;
   password: string;
+  password_confirmation: string;
 }
 
-export interface RegisterRequest {
+export interface PatientRegisterRequest {
+  name: string;
   email: string;
   password: string;
+  birthdate: string;
+  sex: 'male' | 'female' | 'other';
+  //address
+  phone_1:string;
+  phone_2?:string;
+  cpf:string;
+}
+
+export interface ClinicRegisterRequest {
+  name: string;
+  email: string;
+  password: string;
+  cnpj: string;
+  //address
+  phone: string;
+  whatsapp?: string;
+}
+
+export interface SpecialistRegisterRequest {
+  name: string;
+  email: string;
+  password: string;
+  speciality: string;
+  phone: string;
+}
+
+export interface EmployeeRegisterRequest {
+  name: string;
+  email: string;
+  password: string;
+  role: string;
+  phone: string;
 }
