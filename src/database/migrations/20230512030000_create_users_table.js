54b456a3130a23f3bb710a3063516ba2667c3fc4
DenilsonCordeiro <denilsoncordeiro0@gmail.com>
2025-05-21 15:52:47 -0300

    ts -> js :(

---
+++ src/database/migrations/20230512030000_create_users_table.js
@@ -0,0 +1,14 @@
+// migrations/20230512030000_create_users_table.js
+exports.up = function(knex) {
+  return knex.schema.createTable('users', function(table){
+    table.increments('id').primary();      // ID autoincrementável
+    table.string('email', 255).notNullable().unique();  // Email único e obrigatório
+    table.string('password').notNullable();        // Senha obrigatória
+    table.string('role').notNullable();
+    table.timestamps(true, true);                   // Cria campos created_at e updated_at
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('users');  // Remove a tabela caso a migração seja revertida
+};
