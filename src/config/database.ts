da2188d5144aaf44d1ab2b3f8c47835a77e5dddb
DenilsonCordeiro <denilsoncordeiro0@gmail.com>
2025-05-21 15:52:33 -0300

    ts -> js :(

--- src/config/database.ts
+++ src/config/database.ts
@@ -11,7 +11,7 @@ const config: { [key: string]: Knex.Config } = {
         }
       : process.env.DATABASE_URL,
     migrations: {
-      directory: '../dist/database/migrations'
+      directory: './src/database/migrations'
     }
   }
 };
