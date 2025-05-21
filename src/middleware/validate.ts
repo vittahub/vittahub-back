51fe7cb759693d497330e2a3020dda95a93a8620
DenilsonCordeiro <denilsoncordeiro0@gmail.com>
2025-05-21 16:36:31 -0300

    log validate

--- src/middleware/validate.ts
+++ src/middleware/validate.ts
@@ -4,11 +4,14 @@ import { ZodSchema } from "zod";
 
 export const validate = (schema: ZodSchema) =>
 (req: Request, res: Response, next: NextFunction) => {
+    console.log("validate: enter")
     const result = schema.safeParse(req.body);
     if(!result.success) {
         res.status(400);
+        console.log("validate: exit with error")
         return;
     }
     req.body = result.data;
     next();
+    console.log("validate: exit with sucesses")
 };
