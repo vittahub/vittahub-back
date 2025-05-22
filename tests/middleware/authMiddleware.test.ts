8b0d69d5f6fa5c8df12181604130017bb9165c41
Antonio Lisboa de Carvalho <104227926+LisboaAnt@users.noreply.github.com>
2025-05-21 22:45:26 -0300

    Update authMiddleware.test.ts

--- tests/middleware/authMiddleware.test.ts
+++ tests/middleware/authMiddleware.test.ts
@@ -1,18 +1,33 @@
+    import authMiddleware from '../../src/middleware/authMiddleware';
+    import jwt from 'jsonwebtoken';
 
-jest.mock('jsonwebtoken');
+    jest.mock('jsonwebtoken');
 
-describe('authMiddleware', () => {
-    const mockReq = {} as any;
+    describe('authMiddleware', () => {
+    const mockReq = { headers: { authorization: 'Bearer fake_token' } } as any;
     const mockRes = {
-        status: jest.fn().mockReturnThis,
+        status: jest.fn().mockReturnThis(),
         json: jest.fn(),
     } as any;
-    const mockNext = jest.fn()
-
+    const mockNext = jest.fn();
 
     beforeEach(() => {
-    jest.clearAllMocks();
-    process.env.JWT_SECRET = 'test_secret';
+        jest.clearAllMocks();
+        process.env.JWT_SECRET = 'test_secret';
     });
-})
+        
+    it('should call next if token is valid', () => {
+        const mockPayload = { id: '123' };
+        (jwt.verify as jest.Mock).mockReturnValue(mockPayload);
 
+        console.log('Token simulado vai retornar:', mockPayload);
+
+        authMiddleware(mockReq, mockRes, mockNext);
+
+        console.log('mockNext foi chamado?', mockNext.mock.calls.length > 0);
+
+        expect(mockNext).toHaveBeenCalled();
+    });
+
+
+    });
