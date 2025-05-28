e7f93a5ec5b7fc541bd537dfd15faef079b91bd7
DenilsonCordeiro <denilsoncordeiro0@gmail.com>
2025-05-28 09:04:48 -0300

    adding clinic model

---
+++ src/models/clinic.ts
@@ -0,0 +1,10 @@
+import { Address } from "src/types/address";
+
+export type Clinic = {
+    user_id: number;
+    name: string;
+    cnpj: string;
+    address: Address;
+    phone: string;
+    whatsapp: string | null; 
+}
