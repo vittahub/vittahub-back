9e56e63de5121fc979e3aa1d694c633f4692f761
DenilsonCordeiro <denilsoncordeiro0@gmail.com>
2025-05-28 11:00:55 -0300

    only do migration if characteristic doenst exist

--- src/database/migrations/20250521640_create_patients_table.js
+++ src/database/migrations/20250521640_create_patients_table.js
@@ -1,22 +1,26 @@
 // migrations/20250521640_create_patients_table.js
 exports.up = function(knex) {
-  return knex.schema.createTable('patients', function(table){
-    table.increments('id').primary();      
-    table.integer('user_id')
-         .unsigned()
-         .notNullable()
-         .references('id')
-         .inTable('users')
-         .onDelete('CASCADE');      
-    table.string('name', 100).notNullable();
-    table.date('birthdate').notNullable();
-    table.enu('sex', ['male', 'female', 'other']).notNullable();
-    table.jsonb('address').notNullable();
-    table.string('phone_1').notNullable();
-    table.string('phone_2').nullable();
-    table.string('cpf', 14).notNullable().unique();
-    table.timestamps(true, true);                   
-  })
+  return knex.schema.hasTable('patients').then(function(exists){
+    if(!exists){
+      return knex.schema.createTable('patients', function(table){
+        table.increments('id').primary();      
+        table.integer('user_id')
+             .unsigned()
+             .notNullable()
+             .references('id')
+             .inTable('users')
+             .onDelete('CASCADE');      
+        table.string('name', 100).notNullable();
+        table.date('birthdate').notNullable();
+        table.enu('sex', ['male', 'female', 'other']).notNullable();
+        table.jsonb('address').notNullable();
+        table.string('phone_1').notNullable();
+        table.string('phone_2').nullable();
+        table.string('cpf', 14).notNullable().unique();
+        table.timestamps(true, true);                   
+      });  
+    }
+  });
 }
 
 exports.down = function(knex) {
--- src/database/migrations/20250528912_create_clinic_table.js
+++ src/database/migrations/20250528912_create_clinic_table.js
@@ -1,19 +1,23 @@
 exports.up = function(knex) {
-  return knex.schema.createTable('clinics', function(table){
-    table.increments('id').primary();      
-    table.integer('user_id')
-         .unsigned()
-         .notNullable()
-         .references('id')
-         .inTable('users')
-         .onDelete('CASCADE');      
-    table.string('name', 100).notNullable();
-    table.string('cnpj', 14).notNullable().unique();
-    table.jsonb('address').notNullable();
-    table.string('phone').notNullable();
-    table.string('whatsapp').nullable();
-    table.timestamps(true, true);                   
-  })
+  return knex.schema.hasTable('clinics').then(function(exists){
+    if(!exists) {
+      return knex.schema.createTable('clinics', function(table){
+      table.increments('id').primary();      
+      table.integer('user_id')
+           .unsigned()
+           .notNullable()
+           .references('id')
+           .inTable('users')
+           .onDelete('CASCADE');      
+      table.string('name', 100).notNullable();
+      table.string('cnpj', 14).notNullable().unique();
+      table.jsonb('address').notNullable();
+      table.string('phone').notNullable();
+      table.string('whatsapp').nullable();
+      table.timestamps(true, true);                   
+      });
+    }
+  });
 }
 
 exports.down = function(knex) {
