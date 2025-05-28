bd674b602c000c8203072c5aa812617c2bf2d5e2
DenilsonCordeiro <denilsoncordeiro0@gmail.com>
2025-05-28 10:26:39 -0300

    only create if it doesnt exist

--- src/database/migrations/20250512182732_create_users_table.js
+++ src/database/migrations/20250512182732_create_users_table.js
@@ -1,10 +1,14 @@
 // migrations/20230512030000_create_users_table.js
 exports.up = function(knex) {
-  return knex.schema.createTable('users', function(table){
-    table.increments('id').primary();      // ID autoincrementável
-    table.string('email', 255).notNullable().unique();  // Email único e obrigatório
-    table.string('password').notNullable();        // Senha obrigatória
-    table.timestamps(true, true);                   // Cria campos created_at e updated_at
+  return knex.schema.hasTable('users').then(function(exists) {
+    if (!exists) {
+      return knex.schema.createTable('users', function(table) {
+        table.increments('id').primary();                         // ID autoincrementável
+        table.string('email', 255).notNullable();                 // Email único e obrigatório
+        table.string('password', 255).notNullable();              // Senha obrigatória
+        table.timestamps(true, true);                             // Cria campos created_at e updated_at
+      });
+    }
   });
 };
 
